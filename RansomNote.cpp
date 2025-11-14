7d84ddf3a3cf955a0ee3f8a4c9d03ab15db4f1aa
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-11-14 23:51:02 +0530

    Create RansomNote.cpp

---
+++ RansomNote.cpp
@@ -0,0 +1,14 @@
+bool canConstruct(char* ransomNote, char* magazine) {
+    int magcount[26]={0};
+    for(int i=0;magazine[i]!='\0';i++){
+        magcount[magazine[i] - 'a']++;
+    }
+    for(int i=0;ransomNote[i] != '\0';i++){
+        int letter =ransomNote[i] - 'a';
+        if(magcount[letter]==0){
+        return false;
+    }
+    magcount[letter]--;
+}
+return true;
+}
