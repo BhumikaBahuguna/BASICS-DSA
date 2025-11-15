cdb5c646250543a5f745e7fe4595e5c03b9c8e80
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2025-11-15 11:42:17 +0530

    Add files via upload

---
+++ ringsandrods.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countpairs(string rings){
+    int rods[10]={0};
+        int n=rings.size();
+        for(int i=0;i<n;i+=2){
+            int colorbit=rings[i]=='R' ?1:rings[i]=='G'?2:4;
+        rods[rings[i+1]-'0']|=colorbit;
+            }
+            int count=0;
+            for(int i=0;i<10;i++){
+                if(rods[i]==7) count++;
+            }
+            return count;
+    }
+    int main(){
+        string rings="B0R0G0R9G9";
+        cout<<countpairs(rings);
+        return 0;
+    }
